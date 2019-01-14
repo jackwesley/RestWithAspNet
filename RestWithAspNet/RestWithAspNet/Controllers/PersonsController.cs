6fe4cb7c430c3f45bcb095bdf0bc1d9949d10f0f
Jack Wesley Moreira <jackwesley@hotmail.com>
2019-01-14 21:38:35 -0200

    Implementando controler usuários com crud em mock

--- RestWithAspNet/RestWithAspNet/Controllers/PersonsController.cs
+++ RestWithAspNet/RestWithAspNet/Controllers/PersonsController.cs
@@ -1,45 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithAspNet.Model;
+using RestWithAspNet.Services;
 
 namespace RestWithAspNet.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PersonsController : ControllerBase
     {
+        private IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_personService.FindAll());
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<string> Get(long id)
         {
-            return "value";
+            var person = _personService.FindById(id);
+            if (person == null)
+                return NotFound();
+
+            return Ok(person);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Person person)
         {
+            if (person == null)
+                return BadRequest();
+
+            return new ObjectResult(_personService.Create(person));
+
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
         {
+            if (person == null)
+                return BadRequest();
+
+            return new ObjectResult(_personService.Update(person));
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            _personService.Delete(id);
+
+            return NoContent();
         }
     }
 }
---
+++ RestWithAspNet/RestWithAspNet/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using RestWithAspNet.Model;
+
+namespace RestWithAspNet.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person; ;
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "Jack",
+                LastName = "Wesley",
+                Adress = "Rua Sabino,239 SP Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 1; i < 9; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = $"Person Name {i}",
+                LastName = $"Person Last Name {i}",
+                Adress = "Algum lugar do Brasil",
+                Gender = (i%2 == 0) ? "Male" : "Female"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public void Delete(long id)
+        {
+        }
+    }
+}
